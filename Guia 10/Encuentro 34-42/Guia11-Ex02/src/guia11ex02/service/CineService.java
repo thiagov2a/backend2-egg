bc417deeb8811b74a6db3326bc583bd4f2aa26e9
Thiago Vildosa <tvildosa@gmail.com>
2023-05-23 21:23:28 -0300

    subida de ejercicios

--- Guia 10/Encuentro 34-42/Guia11-Ex02/src/guia11ex02/service/CineService.java
+++ Guia 10/Encuentro 34-42/Guia11-Ex02/src/guia11ex02/service/CineService.java
@@ -5,10 +5,10 @@
 package guia11ex02.service;
 
 import guia11ex02.entity.Asiento;
+import guia11ex02.entity.Pelicula;
 import guia11ex02.entity.Sala;
 import guia11ex02.enums.Columna;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -27,7 +27,16 @@ public class CineService {
     }
 
     public void crearSala() {
-        Sala sala = new Sala();
+        Asiento[][] asientos = generarAsientos();
+        Pelicula pelicula = generarPelicula();
+        
+        System.out.print("Ingrese precio de la sala.\n> $");
+        Double precio = validarDouble();
+        
+        salas.add(new Sala(asientos, pelicula, precio));
+    }
+    
+    private Asiento[][] generarAsientos() {
         Asiento[][] asientos = new Asiento[8][6];
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 6; j++) {
@@ -35,15 +44,55 @@ public class CineService {
                 asientos[i][j] = new Asiento(ubicacion);
             }
         }
-        sala.setAsientos(asientos);
-
-        for (Asiento[] fila : asientos) {
-            for (Asiento asiento : fila) {
-                System.out.print(asiento + "|");
+        return asientos;
+    }
+    
+    private Pelicula generarPelicula() {
+        System.out.print("Ingrese titulo de la pelicula.\n> ");
+        String titulo = validarString();
+        
+        System.out.print("Ingrese duracion de la pelicula en horas.\n> ");
+        Integer duracion = validarEntero();
+        
+        System.out.print("Ingresar edad minima de la pelicula.\n> ");
+        Integer edadMinima = validarEntero();
+        
+        System.out.print("Ingrese nombre del director de la pelicula.\n> ");
+        String director = validarString();
+        
+        return new Pelicula(titulo, duracion, edadMinima, director);
+    }
+    
+    
+    
+    private int validarEntero() {
+        while (true) {
+            try {
+                return Integer.parseInt(input.next());
+            } catch (NumberFormatException e) {
+                System.out.print("Ingrese un número entero válido.\n> ");
             }
-            System.out.println();
         }
+    }
 
+    private double validarDouble() {
+        while (true) {
+            try {
+                String entrada = input.next().replace(",", ".");
+                return Double.parseDouble(entrada);
+            } catch (NumberFormatException e) {
+                System.out.print("Ingrese un número válido.\n> ");
+            }
+        }
+    }
+
+    private String validarString() {
+        String entrada = input.next();
+        while (entrada.isEmpty()) {
+            System.out.print("Ingrese un valor válido.\n> ");
+            entrada = input.next();
+        }
+        return entrada;
     }
 
 }
