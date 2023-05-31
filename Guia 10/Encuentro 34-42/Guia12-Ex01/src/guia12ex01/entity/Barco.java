06d927045453c8c932884c8b2400abeec87d32e1
Thiago Vildosa <tvildosa@gmail.com>
2023-05-31 19:49:30 -0300

    subida de ejercicios

---
+++ Guia 10/Encuentro 34-42/Guia12-Ex01/src/guia12ex01/entity/Barco.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guia12ex01.entity;
+
+/**
+ *
+ * @author Thiago
+ */
+public class Barco {
+    
+}
